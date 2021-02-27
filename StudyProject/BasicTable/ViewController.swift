b46bbd63135bef74953e4f6c92b37b2eb5e6eed2
yoonbr <yoon_br@naver.com>
2021-02-28 00:29:20 +0900

    2026 TableView

---
+++ StudyProject/BasicTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicTable
+//
+//  Created by boreum yoon on 2021/02/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
