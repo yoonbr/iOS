e73ee772fb7738d7aaed7affc9ed6670fc8c6b12
yoonbr <yoon_br@naver.com>
2021-04-07 17:50:37 +0900

    0407 Basic Setting

---
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by boreum yoon on 2021/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
