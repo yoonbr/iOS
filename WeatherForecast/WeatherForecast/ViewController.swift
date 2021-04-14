5e38fcf2fe3dc76e25adcc14d2cb651fb76822ba
yoonbr <yoon_br@naver.com>
2021-04-14 23:15:22 +0900

    0414 table view data source

--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var listTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // section parameter로 분기
+        switch section {
+        case 0:
+            return 1
+            
+        case 1:
+            // 예보의 숫자만큼 return
+            return 0
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SummaryTableViewCell", for: indexPath) as! SummaryTableViewCell
+            
+            return cell
+        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ForecastTableViewCell", for: indexPath) as! ForecastTableViewCell
+        
+        return cell
+    }
+    
+    
+    // 2개의 section 표시 - 첫번째 섹션 : 현재 날씨, 두번째 섹션 : 날씨 예보
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
 }
 
