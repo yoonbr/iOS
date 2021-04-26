5de3575da09c45f038499da5ae0b18ccfaeb6714
yoonbr <yoon_br@naver.com>
2021-04-26 23:11:36 +0900

    0426 new project

---
+++ MyMemo/MyMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMemo
+//
+//  Created by boreum yoon on 2021/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
