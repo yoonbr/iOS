24f0d4fdd58507a44e40b226e74843aa0def533d
yoonbr <yoon_br@naver.com>
2021-04-26 23:52:41 +0900

    0426 add memo class

---
+++ MyMemo/MyMemo/Model/Model.swift
@@ -0,0 +1,27 @@
+//
+//  Model.swift
+//  MyMemo
+//
+//  Created by boreum yoon on 2021/04/26.
+//
+
+import Foundation
+
+// 새로운 메모클래스 선언
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    // 속성을 초기화하는 생성자
+    init(content: String){
+        self.content = content
+        // 현재날짜를 바로 저장하기 때문에 별도의 파라미터로 받을 필요 없음
+        insertDate = Date()
+    }
+    
+    // 테이블뷰에 표시할 더미데이터를 배열에 미리 저장
+    static var dummyMemoList = [
+        Memo(content: "Study"),
+        Memo(content: "Sleep😪")
+    ]
+}
