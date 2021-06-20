5e12b70f3e4bc316ffddec758c5f7ea2d21c9720
yoonbr <yoon_br@naver.com>
2021-06-20 23:57:45 +0900

    add Albums api

--- CloneApp/SpotifyClone/SpotifyClone/Managers/APICaller.swift
+++ CloneApp/SpotifyClone/SpotifyClone/Managers/APICaller.swift
@@ -20,6 +20,36 @@ final class APICaller {
         case failedToGetData
     }
     
+    // MARK: - Albums
+    
+    public func getAlbumDetails(for album: Album, completion: @escaping (Result<String, Error>) -> Void) {
+        createRequest(with: URL(string: Constants.baseAPIURL + "/albums/" + album.id),
+                      type: .GET
+        ) { request in
+            let task = URLSession.shared.dataTask(with: request) { data, _, error in
+                guard let data = data, error == nil else {
+                completion(.failure(APIError.failedToGetData))
+                return
+            }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                print(json)
+            }
+            
+            catch {
+                print(error)
+                completion(.failure(error))
+                }
+            }
+        task.resume()
+        }
+    }
+    
+    // MARK: - Playlists
+    
+    // MARK: - Profile
+    
     // 유저 프로필 API
     public func getCurrentUserProfile(completion: @escaping ((Result<UserProfile, Error>) -> Void)) {
         createRequest(
@@ -44,6 +74,8 @@ final class APICaller {
             task.resume()
         }
     }
+    
+    // MARK: - Browse
     
     // 최신곡 API
     public func getNewReleases(completion: @escaping ((Result<NewReleasesResponse, Error>) -> Void)) {
