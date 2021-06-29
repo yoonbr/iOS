52511fca1c2d24936feef01f6b2706c3bf2176e1
yoonbr <yoon_br@naver.com>
2021-06-30 02:15:27 +0900

    basic searchController

--- CloneApp/SpotifyClone/SpotifyClone/Controllers/Core/SearchViewController.swift
+++ CloneApp/SpotifyClone/SpotifyClone/Controllers/Core/SearchViewController.swift
@@ -7,3 +7,22 @@
 
 import UIKit
 
+class SearchViewController: UIViewController {
+    
+    let searchController: UISearchController = {
+        let results = UIViewController()
+        results.view.backgroundColor = .red
+       let vc = UISearchController(searchResultsController: results)
+        vc.searchBar.placeholder = "Songs, Artists, Albums"
+        vc.searchBar.searchBarStyle = .minimal
+        vc.definesPresentationContext = true
+        return vc
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        navigationItem.searchController = searchController
+    }
+}
