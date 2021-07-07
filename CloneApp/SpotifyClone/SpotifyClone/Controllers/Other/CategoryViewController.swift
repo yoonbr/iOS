8024e6abf972d7873c1bb0cde5d47f99f9c4b257
yoonbr <yoon_br@naver.com>
2021-07-07 16:06:22 +0900

    edit CategoryVC

--- CloneApp/SpotifyClone/SpotifyClone/Controllers/Other/CategoryViewController.swift
+++ CloneApp/SpotifyClone/SpotifyClone/Controllers/Other/CategoryViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 class CategoryViewController: UIViewController {
     let category: Category
     
+    private let collectionView = UICollectionView(
+        frame: .zero,collectionViewLayout:
+            UICollectionViewCompositionalLayout(sectionProvider: { _, _ -> NSCollectionLayoutSection? in
+                let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+                
+                item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+                
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: NSCollectionLayoutSize(
+                        widthDimension: .fractionalWidth(1),
+                        heightDimension: .absolute(140)
+                    ),
+                    subitem: item,
+                    count: 2
+                )
+                
+                group.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+                
+                return NSCollectionLayoutSection(group: group)
+            }))
+    
+    // MARK: - init
+    
+    
     init(category: Category) {
         self.category = category
         super.init(nibName: nil, bundle: nil)
@@ -29,18 +53,55 @@ class CategoryViewController: UIViewController {
 
         title = category.name
         
-        APICaller.shared.getCategoryPlaylists(category: category) { result in
-            switch result {
-            case .success(let playlists):
-                self?.playlists = playlists
-            case .failure(let error):
-                print(error.localizedDescription)
+        view.addSubview(collectionView)
+        view.backgroundColor = .systemBackground
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(FeaturedPlaylistCollectionViewCell.self,
+                                forCellWithReuseIdentifier: FeaturedPlaylistCollectionViewCell.identifier
+        )
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        APICaller.shared.getCategoryPlaylists(category: category) { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let playlists):
+                    self?.playlists = playlists
+                    self?.collectionView.reloadData()
+                     
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
             }
         }
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
     }
 
+}
+
+extension CategoryViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return playlists.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: FeaturedPlaylistCollectionViewCell.identifier,
+                for: indexPath) as? FeaturedPlaylistCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        let playlist = playlists[indexPath.row]
+        cell.configure(with: FeaturedPlaylistCellViewModel(
+            name: playlist.name,
+            artworkURL: URL(string: playlist.images.first?.url ?? ""),
+            createName: playlist.owner.display_name
+             )
+        )
+        return cell
+    }
 }
