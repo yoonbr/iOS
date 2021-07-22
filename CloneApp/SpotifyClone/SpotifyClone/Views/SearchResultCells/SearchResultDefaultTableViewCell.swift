262ad17f1f0d32b8a653b4420d484c3b90617fae
yoonbr <yoon_br@naver.com>
2021-07-22 23:54:39 +0900

    add SearchResultDefaultTableViewCell

---
+++ CloneApp/SpotifyClone/SpotifyClone/Views/SearchResultCells/SearchResultDefaultTableViewCell.swift
@@ -0,0 +1,60 @@
+//
+//  SearchResultDefaultTableViewCell.swift
+//  SpotifyClone
+//
+//  Created by boreum yoon on 2021/07/22.
+//
+
+import UIKit
+import SDWebImage
+
+struct SearchResultDefaultTableViewCellViewModel {
+    let title: String
+    let imageURL: URL?
+}
+
+class SearchResultDefaultTableViewCell: UITableViewCell {
+    static let identifier = "SearchResultDefaultTableViewCell"
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(label)
+        contentView.addSubview(iconImageView)
+        contentView.clipsToBounds = true
+        accessoryType = .disclosureIndicator
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        iconImageView.frame = CGRect(x: 10, y: 0, width: contentView.width, height: contentView.height)
+        label.frame = CGRect(x: iconImageView.right+10, y: 0, width: contentView.width-iconImageView.right-15, height: contentView.height)
+
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        iconImageView.image = nil
+        label.text = nil
+    }
+    
+    func configure(with viewModel: SearchResultDefaultTableViewCellViewModel) {
+        label.text = viewModel.title
+        iconImageView.sd_setImage(with: viewModel.imageURL, completed: nil)
+    }
+}
