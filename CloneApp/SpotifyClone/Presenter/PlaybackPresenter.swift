826e898551414e44d325e76e10a002f047ded1c0
yoonbr <yoon_br@naver.com>
2021-08-11 23:22:01 +0900

    add PlaybackPresenter

---
+++ CloneApp/SpotifyClone/Presenter/PlaybackPresenter.swift
@@ -0,0 +1,34 @@
+//
+//  PlaybackPresenter.swift
+//  SpotifyClone
+//
+//  Created by boreum yoon on 2021/08/12.
+//
+
+import Foundation
+import UIKit
+
+final class PlaybackPresenter {
+    
+    static func startPlayback(
+        from viewController: UIViewController,
+        track: AudioTrack
+    ) {
+        let vc = PlayerViewController()
+    }
+    
+    static func startPlayback(
+        from viewController: UIViewController,
+        album: Album
+    ) {
+          
+    }
+    
+    static func startPlayback(
+        from viewController: UIViewController,
+        playlist: Playlist
+    ) {
+          
+    }
+    
+}
