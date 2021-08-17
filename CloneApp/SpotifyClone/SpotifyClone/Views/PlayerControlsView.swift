b25f7f8fd3e8a30ad5d1a7f8b189ed745ac73183
yoonbr <yoon_br@naver.com>
2021-08-17 16:58:47 +0900

    PlayerView UI Setting

--- CloneApp/SpotifyClone/SpotifyClone/Views/PlayerControlsView.swift
+++ CloneApp/SpotifyClone/SpotifyClone/Views/PlayerControlsView.swift
@@ -10,9 +10,72 @@ import UIKit
 
 final class PlayerControlsView: UIView {
     
+    // slider
+    private let volumeSlider: UISlider = {
+        let slider = UISlider()
+        slider.value = 0.5
+        return slider
+    }()
+    
+    // label
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Song"
+        label.numberOfLines = 1
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        return label
+    }()
+    
+    private let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "test"
+        label.numberOfLines = 1
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.textColor = .secondaryLabel
+        return label
+    }()
+    
+    // button
+    private let backButton: UIButton = {
+        let button = UIButton()
+        button.tintColor = .label
+        let image = UIImage(systemName: "backward.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 34, weight: .regular))
+        button.setImage(image, for: .normal)
+        return button
+    }()
+    
+    private let nextButton: UIButton = {
+        let button = UIButton()
+        button.tintColor = .label
+        let image = UIImage(systemName: "forward.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 34, weight: .regular))
+        button.setImage(image, for: .normal)
+        return button
+    }()
+    
+    private let playPauseButton: UIButton = {
+        let button = UIButton()
+        button.tintColor = .label
+        let image = UIImage(systemName: "pause.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 34, weight: .regular))
+        button.setImage(image, for: .normal)
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .red
+        backgroundColor = .clear
+         
+        addSubview(nameLabel)
+        addSubview(subtitleLabel)
+        
+        addSubview(volumeSlider)
+        
+        addSubview(backButton)
+        addSubview(nextButton)
+        addSubview(playPauseButton)
+        
+        // clipsToBounds
+        clipsToBounds = true
+        
     }
     
     required init?(coder: NSCoder) {
@@ -21,5 +84,16 @@ final class PlayerControlsView: UIView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        
+        // slider, label, button size
+        nameLabel.frame = CGRect(x: 0, y: 0, width: width, height: 50)
+        subtitleLabel.frame = CGRect(x: 0, y: nameLabel.bottom+10, width: width, height: 50)
+        
+        volumeSlider.frame = CGRect(x: 10, y: subtitleLabel.bottom+20, width: width-20, height: 44)
+        
+        let buttonSize: CGFloat = 60
+        playPauseButton.frame = CGRect(x: (width - buttonSize)/2, y: volumeSlider.bottom + 30, width: buttonSize, height: buttonSize)
+        backButton.frame = CGRect(x: playPauseButton.left-80-buttonSize, y: playPauseButton.top, width: buttonSize, height: buttonSize)
+        nextButton.frame = CGRect(x: playPauseButton.right+80, y: playPauseButton.top, width: buttonSize, height: buttonSize)
     }
 }
