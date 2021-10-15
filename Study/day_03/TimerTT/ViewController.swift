c72f29c5b5bae76e5c19a81d6a9e1c1dae5a86c8
yoonbr <yoon_br@naver.com>
2021-10-15 19:34:16 +0900

    타이머 프로젝트 수정

--- Study/day_03/TimerTT/ViewController.swift
+++ Study/day_03/TimerTT/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // 1. tf 가져오기
+    @IBOutlet var countLbl: UITextField!
+    
+    // 3. 눌렀을 때 시간의 값 가져와야함
+    var tf = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
     }
-
-
+    
+    // 4. objc 함수 생성 - 들어올 때 마다 1씩 빼기
+    @objc func updateTime() {
+        tf -= 1
+        
+        // tf가 0보다 작아지면 0으로 강제 변환
+//        if tf < 0 {
+//            tf = 0
+//        }
+        
+        countLbl.text = "\(tf)"
+        
+        // tf가 0일 경우 tt의 실행을 중단시킴
+        if tf <= 0 {
+            tt?.invalidate() // 초기화
+            tt = nil // 객체를 없애버림
+        }
+    }
+    
+    // 5. selector 선언
+    let ts = #selector(updateTime)
+    
+    // scheduledTimer 변수 생성 - Timer
+    // Timer를 받을 멤버 변수
+    var tt:Timer?
+    
+    // 2. 버튼 메소드 생성
+    @IBAction func startBtn(_ sender: UIButton) {
+        tf = Int(countLbl.text!)!
+        
+        // * 찍어보기 - int 형변환
+        // tf에 넣은 값 출력
+        // print(Int(countLbl.text!))
+        
+        // 6. scheduledTimer 실행
+        // 멤버 변수 선언 후 tt에게 값 부여 
+        tt = Timer.scheduledTimer(
+            timeInterval: 1.0, // 1초 간격
+            target: self, // 자신 객체 -> 현재 앱 화면
+            selector: ts, // 2번에서 선언한 객체
+            userInfo: nil, // 사용자 정보
+            repeats: true // 반복
+            )
+    }
 }
 
