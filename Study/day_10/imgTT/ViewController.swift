7263cae6995dff3414bbaa16318c4b1ba4d4747c
yoonbr <yoon_br@naver.com>
2021-11-05 20:38:24 +0900

    Auto Layout

---
+++ Study/day_10/imgTT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  imgTT
+//
+//  Created by boreum yoon on 2021/11/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Study/day_11/day_11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  day_11
+//
+//  Created by boreum yoon on 2021/11/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
