b745be6f3f7dbe078a8263e5825a5b3325f1f2d2
yoonbr <yoon_br@naver.com>
2022-01-01 21:08:48 +0900

    update Study

--- Study/day_12/fileTT/ViewController.swift
+++ Study/day_12/fileTT/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myTF: UITextField!
+    
+    @IBOutlet var myTXT: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    //
+    @IBAction func goSave(_ sender: UIButton) {
+    }
+    
+    // 파일을 읽기
+    @IBAction func goRead(_ sender: UIButton) {
+    }
+    
+    // 기존에 있는 파일 내용 밑에 추가
+    @IBAction func goAppend(_ sender: UIButton) {
+    }
 
 }
 
+// 특정 파일이 특정 위치에 있는지 확인 - 없으면 생성하도록 해주고, 없으면 아무 진행을 하지 말아야 함
+// 기존에 있는 파일을 지워버리고 새로 만드는 방법
+
+// viewDidLoad에서 파일이 있는지 없는지 확인 후 파일을 생성
